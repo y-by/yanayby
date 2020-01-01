9381b243a4523b65d2a6e0dbf03ac6ec2beb272e
Janayby <janayby@gmail.com>
2020-01-01 16:44:27 +0200

    first step to add portfolio carousel

--- dist/js/main.js
+++ dist/js/main.js
@@ -57,3 +57,26 @@ function topFunction() {
   document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 }
 
+
+// Carousel
+
+var carousel = document.querySelector('.carousel');
+var cellCount = 6;
+var selectedIndex = 0;
+
+function rotateCarousel() {
+  var angle = selectedIndex / cellCount * -360;
+  carousel.style.transform = 'translateZ(-288px) rotateY(' + angle + 'deg)';
+}
+
+var prevButton = document.querySelector('.previous-button');
+prevButton.addEventListener( 'click', function() {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+var nextButton = document.querySelector('.next-button');
+nextButton.addEventListener( 'click', function() {
+  selectedIndex++;
+  rotateCarousel();
+});
